7be4972f08b6f7e53b6d7be412912975509f1f12
Filip Stan <filipsunfire@filips-mbp.kpn>
2022-11-05 17:20:24 +0100

    created routes and pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignOut from './pages/SignOut';
+import ForgotPassword from './pages/ForgotPassword';
+
 
 function App() {
   return (
     <>
-<h1> Hello world </h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signout" element={<SignOut />} />
+          <Route path="/forgotpassword" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
     </>
   );
 }
